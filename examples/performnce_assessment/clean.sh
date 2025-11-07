8f52fc074a12d448785462832ea34f3efcbfdc67
EdForti <edoardo.forti@uniroma1.it>
2025-11-07 15:52:04 +0100

    Added example folder for performance assessment

---
+++ examples/performnce_assessment/clean.sh
@@ -0,0 +1,9 @@
+rm *.log
+rm *.dat
+rm *.vtr
+rm *.bak
+rm *.bin
+rm slurm*.out
+rm progress.out
+cp GRID/* .
+rm grid.dat
--- examples/performnce_assessment/submit.sh
+++ examples/performnce_assessment/submit.sh
@@ -4,14 +4,15 @@
 #SBATCH -N 1                 # number of nodes N
 #SBATCH --ntasks-per-node=1  # 12 tasks out of 32, 24*N=48 
 #SBATCH --gpus-per-node=1    # 2 gpus out of 4, 4*N=8 gpus
-#SBATCH -A CNHPC_2021201
+#SBATCH -A IscrC_NIRIS
 #SBATCH --qos=boost_qos_dbg
-#SBATCH --job-name=RSTTest
+#SBATCH --job-name=SS001GPU
 #SBATCH --mem=0
-#SBATCH --exclusive
 #SBATCH --error=job.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dalailamalag@gmail.com
+#SBATCH --exclusive
 
-module purge
 module load profile/global ;
 module load cmake/3.24.3 ;
 module load gcc/11.3.0 ;
@@ -21,4 +22,4 @@ module load openmpi/4.1.4--gcc--11.3.0-cuda-11.8 ;
 module load nvhpc/25.3 ;
 module load hpcx-mpi/2.19 ;
 
-srun /leonardo/home/userexternal/eforti00/Prop-STREAmS2/streams_2.exe > data.dat
+srun /leonardo/home/userexternal/eforti00/PROP_FOLDER/Dev-Prop-STREAmS2/streams_2.exe
